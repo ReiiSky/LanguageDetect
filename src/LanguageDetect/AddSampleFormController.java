ff0437d426c31d99adb31e118fbe5962d67c42d0
Murat Can Uste <muratcanuste@gmail.com>
2016-01-04 17:19:50 +0200

    add sample form

--- src/LanguageDetect/AddSampleFormController.java
+++ src/LanguageDetect/AddSampleFormController.java
@@ -1,14 +1,14 @@
 package LanguageDetect;
 
 import LanguageDetect.DetectLangFacade.DetectLangFacade;
+import LanguageDetect.DetectLangFacade.Sample.Sample;
+import LanguageDetect.DetectLangFacade.Sample.SampleDAOMongo;
+import LanguageDetect.DetectLangFacade.WordList.WordListFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -23,6 +23,51 @@ public class AddSampleFormController {
     public Button addBtn;
 
     public void addSamples(ActionEvent actionEvent) {
-
+        String language = languageTF.getText();
+        String specialChars = specCharTF.getText();
+        String[] links = linksTA.getText().split("\\s+");
+        SampleDAOMongo dao = new SampleDAOMongo();
+        Sample sample = dao.getSample(language);
+        if (sample == null) {
+            try {
+                Document doc = Jsoup.connect(links[0]).get();
+                String context = doc.select("div#mw-content-text").text();
+                sample = new Sample(null, language, specialChars,
+                        new WordListFactory().create("Fullword", context, specialChars),
+                        new WordListFactory().create("Trigram", context, specialChars));
+                links[0] = null;
+                for (String link : links) {
+                    try {
+                        if (link != null) {
+                            Document udoc = Jsoup.connect(link).get();
+                            String ucontext = udoc.select("div#mw-content-text").text();
+                            sample.updateFullword(new WordListFactory().create("Fullword", ucontext, specialChars));
+                            sample.updateTrigram(new WordListFactory().create("Trigram", ucontext, specialChars));
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+                dao.updateSample(sample);
+            } catch (Exception e) {
+            }
+        } else {
+            try {
+                for (String link : links) {
+                    try {
+                        Document udoc = Jsoup.connect(link).get();
+                        String ucontext = udoc.select("div#mw-content-text").text();
+                        sample.updateFullword(new WordListFactory().create("Fullword", ucontext, specialChars));
+                        sample.updateTrigram(new WordListFactory().create("Trigram", ucontext, specialChars));
+                    } catch (Exception e) {
+                    }
+                }
+                dao.updateSample(sample);
+            } catch (Exception ignored) {
+            }
+        }
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Adding Operation");
+        alert.setHeaderText("Done");
+        alert.show();
     }
 }
